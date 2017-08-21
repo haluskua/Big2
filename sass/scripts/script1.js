09fed5850d9f5d8ed96660ef5a81a71619154839
haluskua <haluskua@users.noreply.github.com>
2017-08-22 04:13:44 +1000

    applied scrollTop & smooth scrolling for fixed navigation- still some bugs to fix yet

--- sass/scripts/script1.js
+++ sass/scripts/script1.js
@@ -1,24 +1,85 @@
 
 
-jQuery(document).ready(function($) {
+$(document).ready(function() {
+
+
+    //Get the header height
+    var headerHeight = $('#navtabs').outerHeight();
+
+    $('.icon').click(function(e) {
+      //getting the href attribute
+    var linkHref = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(linkHref).offset().top - headerHeight
+    }, 1060);
+    e.preventDefault();
+  });
 
   /* Stick navigation to the top of the page */
-  var stickyNavTop = $('#navtabs').offset().top;
+  var stickyNavTop = $('.navbar').offset().top;
 
   $(window).scroll(function() {
     var scrollTop = $(window).scrollTop();
 
   /*changing between classes */
     if (scrollTop > stickyNavTop) {
-      $('#navtabs').addClass('sticky');
-      $('#nav').addClass('shifted');
+      $('.navbar').addClass('sticky');
+      $('.hetpage').addClass('shifted');
     } else {
         /*restoring back to normal */
-      $('#navtabs').removeClass('sticky');
-      $('#nav').removeClass('shifted');
+      $('.navbar').removeClass('sticky');
+      $('.hetpage').removeClass('shifted');
     }
   });
 });
+
+
+    // jQuery(document).ready(function($) {
+    //
+    //   /* Stick navigation to the top of the page */
+    //   var stickyNavTop = $('.navbar').offset().top;
+    //
+    //   $(window).scroll(function() {
+    //     var scrollTop = $(window).scrollTop();
+    //
+    //   /*changing between classes */
+    //     if (scrollTop > stickyNavTop) {
+    //       $('.navbar').addClass('sticky');
+    //       $('main').addClass('shifted');
+    //     } else {
+    //         /*restoring back to normal */
+    //       $('.navbar').removeClass('sticky');
+    //       $('main').removeClass('shifted');
+    //     }
+    //   });
+    // });
+//
+//   // smooth scrolling:
+//   $(document).ready(function() {
+//      function filter(string) {
+//         return string
+//            .replace(/^\//,'')
+//            .replace(/(index|default)\.[a-zA-Z]{3,4}$/,'')
+//            .replace(/\/$/,'')
+//      }
+//      $('a[href*=#]').each(function() {
+//         if (filter(location.pathname) == filter(this.pathname)
+//            && location.hostname == this.hostname
+//   	 && this.hash.replace(/#/,'') ) {
+//   	    var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+//   	    var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+//   	    if ($target) {
+//   	       var targetOffset = $target.offset().top;
+//   	       $(this).click(function() {
+//   	          $('html, body').animate({scrollTop: targetOffset}, 400);
+//   	          return false;
+//   	        });
+//               }
+//            }
+//         });
+//      });
+//
+// });
 
 
 
