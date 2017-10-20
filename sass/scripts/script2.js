d629c5ed61fde4dae55723fed69c970c51fa8917
haluskua <haluskua@users.noreply.github.com>
2017-10-20 14:24:36 +1000

    added jquery image slider(mobile responsive) 80%complete

--- sass/scripts/script2.js
+++ sass/scripts/script2.js
@@ -97,16 +97,27 @@ function checkSize(){
             $("#light-box").hide();
           });
 
-          // $(".next").on('click', function(){
-          //   var currentImg = $(".active");
-          //   var nextImg = currentImg.next();
-          //
-          // });
+// jquery image slider
 
+          $('.next').on('click', function(){
+            var currentImg = $('.active');
+            var nextImg = currentImg.next();
 
+            if(nextImg.length){
+              currentImg.removeClass('active').css('z-index', -10);
+              nextImg.addClass('active').css('z-index', 10);
+            }
+          });
 
+          $('.prev').on('click', function(){
+            var currentImg = $('.active');
+            var prevImg = currentImg.prev();
 
-
+            if(prevImg.length){
+              currentImg.removeClass('active').css('z-index', -10);
+              prevImg.addClass('active').css('z-index', 10);
+            }
+          });
 
         // //onclick function for image gallery
         // $('#img1').on("click", function() {
