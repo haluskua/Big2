c73b0927598308df412778246473eb1fce43a0ae
haluskua <haluskua@users.noreply.github.com>
2017-11-22 22:01:17 +1000

    commit for testing

--- sass/scripts/script1.js
+++ sass/scripts/script1.js
@@ -25,60 +25,61 @@ $(document).ready(function(){
           e.preventDefault();
         });
 
-    // code for lightbox
-    $(".img").click(function(){
-      $src=$(this).attr("src");
-        if(!$("#light-box").length > 0) {
-          //creates a new div and includes lightbox properties
-          $("#mygrafix").append("<div id='light-box'><span class='material-icons'>close</span><img src=''></div>");
-          $("#light-box").show();
-          $("#light-box img").attr("src",$src);
 
-          $("#light-box").append("<div id='next'<span class ='next'>-></span></div>");
-          $("#light-box").append("<div id='prev'<span class ='prev'><-</span></div>");
+          // code for lightbox
+          $(".img").click(function(){
+            $src=$(this).attr("src");
+              if(!$("#light-box").length > 0) {
+                //creates a new div and includes lightbox properties
+                $("#mygrafix").append("<div id='light-box'><span class='material-icons'>close</span><img src=''></div>");
+                $("#light-box").show();
+                $("#light-box img").attr("src",$src);
 
-        }else{
-          $("#light-box").show();
-          $("#light-box img").attr("src",$src);
-        }
-      });
+                $("#light-box").append("<div id='next'<span class ='next'>-></span></div>");
+                $("#light-box").append("<div id='prev'<span class ='prev'><-</span></div>");
 
-      $("#mygrafix").on("click", "#light-box span", function() {
-        $("#light-box").hide();
-      });
+              }else{
+                $("#light-box").show();
+                $("#light-box img").attr("src",$src);
+              }
+            });
 
-    // jquery image slider
-      $('.next').on('click', function(){
-        var currentImg = $('.active');
-        var nextImg = currentImg.next();
+            $("#mygrafix").on("click", "#light-box span", function() {
+              $("#light-box").hide();
+            });
+          // jquery image slider
+            $('.next').on('click', function(){
+              var currentImg = $('.active');
+              var nextImg = currentImg.next();
 
-        if(nextImg.length){
-          currentImg.removeClass('active').css('z-index', -10);
-          nextImg.addClass('active').css('z-index', 10);
-        }
-      });
+              if(nextImg.length){
+                currentImg.removeClass('active').css('z-index', -10);
+                nextImg.addClass('active').css('z-index', 10);
+              }
+            });
 
-      $('.prev').on('click', function(){
-        var currentImg = $('.active');
-        var prevImg = currentImg.prev();
+            $('.prev').on('click', function(){
+              var currentImg = $('.active');
+              var prevImg = currentImg.prev();
 
-        if(prevImg.length){
-          currentImg.removeClass('active').css('z-index', -10);
-          prevImg.addClass('active').css('z-index', 10);
-        }
-      });
+              if(prevImg.length){
+                currentImg.removeClass('active').css('z-index', -10);
+                prevImg.addClass('active').css('z-index', 10);
+              }
+            });
 
-
-      //show menu bar onclick on main logo
-      $(".haus-menu, #hetinfo").click(function(){
-        $(".mobile-container").show(500);
-        $(".nav-folders").show(480);
-        $(".haus").hide(500);
-        $(".exit-menu").show(700);
-      });
-      $(".exit-menu, .hamburger-menu").on("click", function(){
-        $(".mobile-container").hide(300);
-        $(".haus").show(300);
-      });
+            if (jQuery(window).width() <= 481) {
+                $(".exit-menu, .hamburger-menu").on("click", function(){
+                  $(".mobile-container").hide(300);
+                  $(".haus").show(300);
+                });
+                //show menu bar onclick on main logo
+                $(".haus-menu, #hetinfo").click(function(){
+                  $(".mobile-container").show(500);
+                  $(".nav-folders").show(480);
+                  $(".haus").hide(500);
+                  $(".exit-menu").show(700);
+                });
+            }
 
 });
---
+++ sass/scripts/script2.js
@@ -0,0 +1,15 @@
+
+
+
+
+//
+// jQuery(document).ready(function(){
+//     if (jQuery(window).width() > 500) {
+//         jQuery(".haus").css("display", "none");
+//     }
+// });
+// jQuery(window).resize(function () {
+//         if (jQuery(window).width() > 500) {
+//             jQuery(".haus").css("display", "none");
+//         }
+// });
